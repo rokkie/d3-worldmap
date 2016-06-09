f05efe8a78c400580fc87259553df231fb05b1dc
Rocco Bruyn <rocco.bruyn@gmail.com>
2016-06-09 11:31:27 +0200

    Exclude locales from moment

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+var webpack = require('webpack');
+
 module.exports = {
   entry: {
     app: ['./index.js']
@@ -14,6 +16,10 @@ module.exports = {
     loaders    : [],
     postLoaders: []
   },
+
+  plugins: [
+    new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /en-gb/)
+  ],
 
   devtool: '#source-map',
 
